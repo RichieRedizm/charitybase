22f5dec8315efbed7f6af85f7e771824cbcb741d
Richie Redizm <richieredizm@gmail.com>
2019-09-12 14:34:55 +0100

    update financies and snapshot to show latest income and spening

--- src/components/atoms/Finances.js
+++ src/components/atoms/Finances.js
@@ -7,8 +7,17 @@ const Finances = ({ finances }) => {
     totalIncome += finance.income
     totalSpending += finance.spending
   })
+
   return (
     <Fragment>
+      <div>
+        <strong className='label-light'>Latest Income: </strong>£
+        {finances[0].income}
+      </div>
+      <div>
+        <strong className='label-light'>Latest Spending: </strong>£
+        {finances[0].spending}
+      </div>
       <div>
         <strong className='label-light'>Total Income: </strong>£{totalIncome}
       </div>
